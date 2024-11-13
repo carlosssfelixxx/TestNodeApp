5a870c458deb6d7edb8b0b929e37a1bc60680b3f
Carlos Felix <carlosfs16@hotmail.com>
2024-11-13 13:37:29 -0600

    test actions

---
+++ app.js
@@ -0,0 +1,7 @@
+const moment = require('moment');
+let i = 5;
+let j = 10;
+let fecha = moment().format("LL");
+console.log("Iniciando...");
+console.log("La suma es ", i + j);
+console.log("Y la fecha es ", fecha);
